9d5e3293fa61d08965328aecd8445c8b31e8aff5
thiago de mattos <205856258+thiago-de-mattos@users.noreply.github.com>
2025-12-18 19:20:34 -0300

    feat: criando button/ children

--- Chronos-pomodoro/src/App.tsx
+++ Chronos-pomodoro/src/App.tsx
@@ -1,13 +1,19 @@
 import { Heading } from './components/Heading';
 import './styles/theme.css'
 import './styles/global.css'
+import { TimerIcon } from 'lucide-react';
 
 export function App() {
-    console.log("oi");
 
     return (
     <>
-        <Heading/> 
+        <Heading >
+            Ola mundo 1 
+            <button>
+                <TimerIcon />
+            </button> 
+        </Heading> 
+
         <p>
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. A, dolore! Nesciunt labore nam iusto, sunt aliquam aut necessitatibus repellat fugit mollitia veritatis culpa obcaecati. Repellat dicta aspernatur illo maxime aliquam.
         </p>
