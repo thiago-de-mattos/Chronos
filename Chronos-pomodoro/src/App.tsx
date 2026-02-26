93c068a82f434636021825844f4fd69ffa8b71b6
thiago de mattos <205856258+thiago-de-mattos@users.noreply.github.com>
2026-02-26 03:02:30 -0300

    feat: adiciona componente de menu

--- Chronos-pomodoro/src/App.tsx
+++ Chronos-pomodoro/src/App.tsx
@@ -1,21 +1,21 @@
+import { Container } from './components/Container';
+import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
+
 import './styles/theme.css';
 import './styles/global.css';
 
-
-import { Heading } from './components/Heading';
-import { Container } from './components/Container';
-
 export function App() {
 
-    return( 
+    return  ( 
         <>
 
             <Container>
-                <Heading>Logo</Heading>
+                <Logo />
             </Container>
 
             <Container>
-                <Heading>Menu</Heading>
+                <Menu />
             </Container>
             
         </>
