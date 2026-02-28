18e25ff6dfb93ebe6671a4afb5591567598d5a36
thiago de mattos <205856258+thiago-de-mattos@users.noreply.github.com>
2026-02-27 22:23:03 -0300

    feat: input com condicionais do jsx

--- Chronos-pomodoro/src/App.tsx
+++ Chronos-pomodoro/src/App.tsx
@@ -2,6 +2,7 @@ import { Container } from './components/Container';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
 import { CountDown } from './components/CountDown';
+import { DefaultInput } from './components/DefaultInput';
 
 import './styles/theme.css';
 import './styles/global.css';
@@ -22,6 +23,29 @@ export function App() {
             <Container>
                 <CountDown />
             </Container>
+
+            <Container>
+                <form className='form' action=''>
+                    <div className='formRow'>
+                        <DefaultInput labelText='qualquer coisa' id='meuInput' type='text' />
+                    </div>
+                
+
+                    <div className='formRow'>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+
+                    <div className='formRow'>
+                        <p>ciclos</p>
+                        <p>0 0 0 0 0 0 </p>
+                    </div>
+
+                    <div className='formRow'>
+                        <button>enviar</button>
+                    </div>
+                </form>
+            </Container>
+
             
         </>
     )
